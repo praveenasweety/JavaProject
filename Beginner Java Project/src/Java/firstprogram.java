5274f7fd970890e2beb8055e98d45ac80831789b
Praveenasweety <81365813+praveenasweety@users.noreply.github.com>
2021-04-02 16:20:03 -0400

    adding java

---
+++ Beginner Java Project/src/Java/firstprogram.java
@@ -0,0 +1,22 @@
+package Java;
+
+public class firstprogram {
+
+	public static void main(String[] args) {
+		boolean flag = true;
+	    System.out.println(flag);  // prints true
+	    byte range;
+	    range = 124;
+	    System.out.println(range);    // prints 124
+	    short temperature;
+	    temperature = -200;
+	    System.out.println(temperature);  // prints -200
+	    char letter = '\u0051';
+	    System.out.println(letter);  // prints Q
+		System.out.println("Hi! World");   //prints Hi! World
+		
+			
+
+	}
+
+}
